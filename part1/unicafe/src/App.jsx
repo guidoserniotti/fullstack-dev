949aff12c2c98574e8812923b8a3b63d55124ba0
guidoserniotti <guido.serniotti.13@gmail.com>
2025-08-18 11:08:29 -0300

    unicafe first commit

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const App = () => {
+    // guarda los clics de cada botón en su propio estado
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    return <div></div>;
+};
+
+export default App;
