11d4d17d309dfde4dbae5225adbd4efa2c74fa6d
guidoserniotti <guido.serniotti.13@gmail.com>
2025-08-23 16:14:34 -0300

    part2 ex 2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,14 +1,17 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ parts }) => {
-    let part1 = parts[0];
-    let part2 = parts[1];
-    let part3 = parts[2];
     return (
         <div>
-            <Part name={part1.name} exercises={part1.exercises} />
-            <Part name={part2.name} exercises={part2.exercises} />
-            <Part name={part3.name} exercises={part3.exercises} />
+            {parts.map((part) => (
+                <Part
+                    key={part.id}
+                    name={part.name}
+                    exercises={part.exercises}
+                />
+            ))}
+            <Total parts={parts} />
         </div>
     );
 };
